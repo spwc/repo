99742047593683b0d234dcd8ded02e83dceb8bb9
spwc <spwc123@gmail.com>
2024-05-03 17:38:47 +0100

    Reworked

---
+++ plugin.program.sptoolbox/uservar.py
@@ -0,0 +1,10 @@
+import xbmcaddon
+
+'''#####-----Build File-----#####'''
+buildfile = 'https://www.dropbox.com/s/1i98cw14bcnr7i1/builds.json?dl=1'
+
+'''#####-----Notifications File-----#####'''
+notify_url  = 'https://www.dropbox.com/s/wzwh1waae3gxfmp/notify.txt?dl=1'
+
+'''#####-----Excludes-----#####'''
+excludes  = ['plugin.video.whatever']
